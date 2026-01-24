aad12818be427c5921f8eda1e8101b0b5ca9a52e
Akash Mishra <akashmishra967033@gmail.com>
2026-01-24 19:02:09 +0530

    add new folder and code

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 app.use(methodOverride("_method"));
 
@@ -28,38 +29,42 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 //index route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync ( async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings })
-});
+}));
 //new route
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs")
 })
 //show route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync (async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-});
+}));
 //create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync (async (req, res,next) => {
+    if(!req.body.listing){
+        throw new ExpressError(404,"Send valid data for listing")
+    }
     const newlisting = new Listing(req.body.listing);
     await newlisting.save();
     res.redirect("/listings");
-});
+    
+}));
 //edit route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync (async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing })
-});
+}));
 //update route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id",wrapAsync ( async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-});
+}));
 //delete route
 app.delete("/listings/:id", async (req, res) => {
     let { id } = req.params;
@@ -81,6 +86,18 @@ app.delete("/listings/:id", async (req, res) => {
 //     console.log("sample was saved");
 //     res.send("successfull");
 // });
+
+// midleware
+
+app.all(/.*/,(req,res,next) =>{
+    next(new ExpressError(404, "Page Not Found !"))
+})
+
+app.use((err,req,res,next) =>{
+    let {statusCode = 500, message ="something went wrong"} = err;
+    res.status(statusCode).render("error.ejs",{message});
+    // res.status(statusCode).send(message);
+});
 
 app.listen("8080", (req, res) => {
     console.log("port is running now")
